0cd08de1d4ad753f2c9d130371c8fee9db8612b1
Isaac Kearse <isaac@kearse.co.nz>
2014-06-16 12:25:17 +1200

    Add new static target

---
+++ vendor/aixmlserialize/AIXMLSerializeStatic/AIXMLSerializeStatic.h
@@ -0,0 +1,13 @@
+//
+//  AIXMLSerializeStatic.h
+//  AIXMLSerializeStatic
+//
+//  Created by Isaac Kearse on 16/06/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AIXMLSerializeStatic : NSObject
+
+@end
---
+++ vendor/aixmlserialize/AIXMLSerializeStatic/AIXMLSerializeStatic.m
@@ -0,0 +1,13 @@
+//
+//  AIXMLSerializeStatic.m
+//  AIXMLSerializeStatic
+//
+//  Created by Isaac Kearse on 16/06/14.
+//
+//
+
+#import "AIXMLSerializeStatic.h"
+
+@implementation AIXMLSerializeStatic
+
+@end
